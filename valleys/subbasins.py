1ba508ece32214d8802453359c8bb1d89396e8eb
Arthur Koehl <avkoehl@ucdavis.edu>
2023-12-18 16:58:41 -0800

    fix logic

--- valleys/subbasins.py
+++ valleys/subbasins.py
@@ -60,7 +60,7 @@ def setup_wbt(whitebox_dir, working_dir):
     working_directory = os.path.abspath(working_dir)
     if os.path.exists(working_directory):
             shutil.rmtree(working_directory)
-            os.mkdir(working_directory)
+    os.mkdir(working_directory)
     wbt.set_working_dir(os.path.abspath(working_directory))
     wbt.set_verbose_mode(False)
     return wbt
