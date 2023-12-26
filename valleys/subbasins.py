bd773e10bf28252aae0a74949ea79c53b05a0b04
Arthur Koehl <avkoehl@ucdavis.edu>
2023-12-25 22:30:19 -0800

    Was having issues with streams extracting well but stream link identifier finding hundreds of streams. FillDepressions is more crude but does seem to work

--- valleys/subbasins.py
+++ valleys/subbasins.py
@@ -15,7 +15,12 @@ This module takes a dem and creates a subbasin shapefile.
 import os
 
 def delineate_streams(wbt, dem_raster_file, threshold):
-    wbt.breach_depressions_least_cost(dem_raster_file, "filled_dem.tif", dist=1000)
+
+    # wbt.breach_depressions_least_cost(dem_raster_file, "filled_dem.tif", 
+    #                                  dist=10, max_cost=None, flat_increment=None,
+    #                                  fill=False, min_dist=True)
+
+    wbt.fill_depressions(dem_raster_file, "filled_dem.tif")
     wbt.d8_pointer("filled_dem.tif", "d8_pntr.tif")
     wbt.d8_flow_accumulation("d8_pntr.tif", "flow_acc.tif", pntr=True)
     wbt.extract_streams("flow_acc.tif", "streams.tif", threshold=threshold)
@@ -28,11 +33,15 @@ def delineate_streams(wbt, dem_raster_file, threshold):
     return os.path.join(wbt.work_dir, "streams.tif")
 
 def delineate_subbasins(wbt, dem_raster_file, streams_raster_file):
-    wbt.breach_depressions_least_cost(dem_raster_file, "filled_dem.tif", dist=1000)
+    #wbt.breach_depressions_least_cost(dem_raster_file, "filled_dem.tif", 
+    #                                  dist=10, max_cost=None, flat_increment=None,
+    #                                  fill=False, min_dist=True)
+    wbt.fill_depressions(dem_raster_file, "filled_dem.tif")
     wbt.d8_pointer("filled_dem.tif", "d8_pntr.tif")
     wbt.subbasins("d8_pntr.tif", streams_raster_file, "subbasins.tif")
 
-    return {'filled_dem': os.path.join(wbt.work_dir, "filled_dem.tif"),
+    return {'dem': dem_raster_file,
+            'filled_dem': os.path.join(wbt.work_dir, "filled_dem.tif"),
             'd8_pntr': os.path.join(wbt.work_dir, "d8_pntr.tif"),
             'subbasins': os.path.join(wbt.work_dir, "subbasins.tif"),
             'streams': streams_raster_file}
