03bda4faa15b1b33e7d311aeadd1fe5f314b4245
Arthur Koehl <avkoehl@ucdavis.edu>
2024-02-03 23:32:37 -0800

    add cross ridge filter

--- valleys/valley.py
+++ valleys/valley.py
@@ -18,9 +18,34 @@ import rioxarray
 import scipy  
 from scipy import signal
 import xarray as xr
+import matplotlib.pyplot as plt
 
 from valleys.cross_section import get_cross_section_points
 from valleys.terrain import compute_terrain_rasters
+
+def filter_ridge_crossing(profile):
+    # check if the profile crosses a ridgeline
+    # if a point has positive curvature, and there is a big jump in HAND relative to the change in elevation
+    # then it is likely that the profile has crossed a ridgeline
+    # remove the points after the ridgeline
+    # sometimes seems that curvature is not always positive when crossing a ridgeline
+    temp = profile.copy()
+    temp['diff'] = temp['alpha'].abs()
+    # sort on diff column
+    temp = temp.sort_values(by='diff', ascending=True)
+
+    temp['hand_diff'] = temp['hand'].diff()
+    temp['elev_diff'] = temp['elevation'].diff()
+    temp['ratio'] = temp['hand_diff'] / temp['elev_diff']
+    temp['ratio'] = temp['ratio'].abs()
+
+    # find the first point where the ratio is greater than 2
+    for i in range(len(temp)):
+        if temp['ratio'].iloc[i] > 3 and temp['hand_diff'].iloc[i] > 20:
+            #            if temp['curvature'].iloc[i] > 0:
+            keep = temp.iloc[:i]
+            return profile.loc[profile['point_id'].isin(keep['point_id'])]
+    return profile
 
 def prepare_xsection(xs_points):
     """
@@ -29,6 +54,9 @@ def prepare_xsection(xs_points):
     1. remove any duplicate points
     2. make sure that there are enough points
     3. recenter the alpha values so that the channel is at 0
+    4. make sure there are points on both sides of the channel
+    5. remove points that are passed a ridgeline
+    6. double check that there are enough points
 
     The cross section points are sampled along a line perpendicular to the 
     centerline, which is generally a smoothed version of the stream centerline
@@ -84,53 +112,74 @@ def prepare_xsection(xs_points):
         min_point = temp['hand'].idxmin()
         min_alpha = temp['alpha'].loc[min_point]
         temp['alpha'] = temp['alpha'] - min_alpha
-    else if len(temp['strm_val'].dropna().unique()) == 1:
+    elif len(temp['strm_val'].dropna().unique()) == 1:
         strm_val = temp['strm_val'].dropna().unique()[0]
         min_point = temp['hand'].loc[temp['strm_val'] == strm_val].idxmin()
         min_alpha = temp['alpha'].loc[min_point]
         temp['alpha'] = temp['alpha'] - min_alpha
-    else if len(temp['strm_val'].dropna().unique()) > 1:
+    elif len(temp['strm_val'].dropna().unique()) > 1:
         stream_points = temp.loc[temp['strm_val'].notna()]
         # find stream point with min abs alpha
         min_point = stream_points['alpha'].abs().idxmin()
         min_alpha = stream_points['alpha'].loc[min_point]
         temp['alpha'] = temp['alpha'] - min_alpha
 
+    # 4. make sure there are points on both sides of the channel
+    if len(temp.loc[temp['alpha'] < 0]) == 0 or len(temp.loc[temp['alpha'] > 0]) == 0:
+        return None
+
+    # 5. remove points that are passed a ridgeline
+    temp_neg = filter_ridge_crossing(temp.loc[temp['alpha'] <= 0])
+    temp_pos = filter_ridge_crossing(temp.loc[temp['alpha'] >= 0])
+    temp = pd.concat([temp_neg, temp_pos])
+    # remove duplicate points
+    temp = temp.drop_duplicates(subset=['point_id'])
+
+    # 6. double check that there are enough points
+    if len(temp) < 5:
+        return None
+
+    if len(temp.loc[temp['alpha'] < 0]) == 0 or len(temp.loc[temp['alpha'] > 0]) == 0:
+        return None
     return temp
 
 def find_half_profile_break_point(profile, peak_ids, slope_threshold=20):
+    profile = profile.copy()
     profile = profile.reindex(profile['alpha'].abs().sort_values().index)
-    key_points = profile.loc[profile['point_id'].isin(peak_ids)]
+    profile['int_index'] = np.arange(len(profile))
 
     # add the first and last points
-    key_points = key_points.append(profile.iloc[0])
-    key_points = key_points.append(profile.iloc[-1])
-    # remove duplicates
+    key_point_ids = peak_ids.copy()
+    key_point_ids.insert(0, profile.iloc[0]['point_id'])
+    key_point_ids.append(profile.iloc[-1]['point_id'])
+    key_points = profile.loc[profile['point_id'].isin(key_point_ids)]
     key_points = key_points.drop_duplicates(subset=['alpha'])
 
     # get mean slope between each pair of points
     # foreach pair of points, get all the points between them and get mean of slope column
     slopes = []
     for ind, (label, row) in enumerate(key_points.iterrows()):
-        point1 = row
-        point2 = key_points.iloc[ind+1]
+        if ind == len(key_points) - 1:
+            break
+        point_id_1 = row['point_id']
+        point_id_2 = key_points.iloc[ind+1]['point_id']
 
-        # positions in the profile
-        pos1 = profile.index.get_loc(point1['point_id'])
-        pos2 = profile.index.get_loc(point2['point_id'])
+        pos1 = profile.loc[profile['point_id'] == point_id_1]['int_index'].values[0]
+        pos2 = profile.loc[profile['point_id'] == point_id_2]['int_index'].values[0]
+
         points = profile.iloc[pos1:pos2]
         mean_slope = points['slope'].mean()
-        slopes.append(point1['point_id'], mean_slope)
+        slopes.append((point_id_1, mean_slope))
     slopes = pd.DataFrame(slopes, columns=['point_id', 'mean_slope'])
 
     # get first index where slope > slope_threshold
     # else return None
     exceeds = slopes['mean_slope'] > slope_threshold
     if exceeds.any():
-        return slopes.loc[exceeds].iloc[0]
+        return slopes.loc[exceeds].iloc[0]['point_id']
     return 
 
-def find_xs_break_points(df, peak_threshold=0.002, slope_threshold=20)
+def find_xs_break_points(df, peak_threshold=0.002, slope_threshold=20):
     """ df is the points for a single cross section 
     has columns: alpha, curvature, hand, point_id, cross_section_id, hillslope, slope, strm_val
 
@@ -140,22 +189,21 @@ def find_xs_break_points(df, peak_threshold=0.002, slope_threshold=20)
 
     # remove duplicates, recenter alpha, and make sure there are enough points
     df = prepare_xsection(df)
-    df['dist'] = df['alpha'].abs()
 
     # if the cross section is not valid, return None
     if df is None:
-        return (None, None)
+        return (None, None, None)
 
     # find the peaks in the curvature profile
     # if done after splitting, may not find the peak at the first point in the series (alpha == 0)
     # wheras if series is considered as a whole, the peak might be found, so do it before splitting
     peak_inds, _ =  signal.find_peaks(-df['curvature'], height=peak_threshold)
-    peak_ids = df['point_id'].iloc[peak_inds]
+    peak_ids = df['point_id'].iloc[peak_inds].tolist()
 
     pos_bp = find_half_profile_break_point(df.loc[df['alpha'] >=0], peak_ids, slope_threshold)
     neg_bp = find_half_profile_break_point(df.loc[df['alpha'] <= 0], peak_ids, slope_threshold)
 
-    return (pos_bp, neg_bp, peak_ids.tolist())
+    return (pos_bp, neg_bp, peak_ids)
 
 def determine_threshold(break_points):
     threshold = break_points['hand'].quantile(.6)
@@ -247,7 +295,6 @@ def close_holes(poly):
             return Polygon(list(poly.exterior.coords))
         return poly
 
-# TODO: redo to use the modified find_xs_break_points function
 def get_break_points(points):
     """
     Find the break points for each cross section
@@ -260,12 +307,47 @@ def get_break_points(points):
 
     break_points = []
     for xs_id in points['cross_section_id'].unique():
-        pos, neg, peaks = find_xs_break_points(points, xs_id)
+        pos, neg, peaks = find_xs_break_points(points.loc[points['cross_section_id'] == xs_id])
         break_points.append(
-            (xs_id, pos, neg, peaks)
+            (xs_id, pos, neg, peaks))
 
-    break_points = pd.DataFrame(break_points, columns=['cross_section_id', 'pos', 'neg', 'peak_ids'])
+    break_points_df = pd.DataFrame(break_points, columns=['cross_section_id', 'pos', 'neg', 'peak_ids'])
+    combined = break_points_df['pos'].dropna().to_list() + break_points_df['neg'].dropna().to_list()
+
+    break_points = points.loc[points['point_id'].isin(combined)]
+    # break_points.to_file('break_points.shp')
     return break_points
+
+
+def plot_xs_break_points(df, pos, neg, peaks):
+    #xs_id = 3
+    #df = points.loc[points['cross_section_id'] == xs_id]
+    #pos, neg, peaks = find_xs_break_points(df)
+    #plot_xs_break_points(df, pos, neg, peaks)
+    ##        # df is full profile of xs
+    df = prepare_xsection(df)
+    fig, ax = plt.subplots(4,1)
+
+    ax[0].scatter(df['alpha'], df['elevation'])
+    ax[0].scatter(df.loc[df['point_id'].isin(peaks), 'alpha'], df.loc[df['point_id'].isin(peaks), 'elevation'], c='b')
+    ax[0].scatter(df.loc[df['point_id'] == pos, 'alpha'], df.loc[df['point_id'] == pos, 'elevation'], c='r')
+    ax[0].scatter(df.loc[df['point_id'] == neg, 'alpha'], df.loc[df['point_id'] == neg, 'elevation'], c='g')
+
+    ax[1].scatter(df['alpha'], df['hand'])
+    ax[1].scatter(df.loc[df['point_id'].isin(peaks), 'alpha'], df.loc[df['point_id'].isin(peaks), 'hand'], c='b')
+    ax[1].scatter(df.loc[df['point_id'] == pos, 'alpha'], df.loc[df['point_id'] == pos, 'hand'], c='r')
+    ax[1].scatter(df.loc[df['point_id'] == neg, 'alpha'], df.loc[df['point_id'] == neg, 'hand'], c='g')
+
+    ax[2].plot(df['alpha'], df['slope'])
+    ax[2].scatter(df.loc[df['point_id'].isin(peaks), 'alpha'], df.loc[df['point_id'].isin(peaks), 'slope'], c='b')
+    ax[2].scatter(df.loc[df['point_id'] == pos, 'alpha'], df.loc[df['point_id'] == pos, 'slope'], c='r')
+    ax[2].scatter(df.loc[df['point_id'] == neg, 'alpha'], df.loc[df['point_id'] == neg, 'slope'], c='g')
+
+    ax[3].plot(df['alpha'], df['curvature'])
+    ax[3].scatter(df.loc[df['point_id'].isin(peaks), 'alpha'], df.loc[df['point_id'].isin(peaks), 'curvature'], c='b')
+    ax[3].scatter(df.loc[df['point_id'] == pos, 'alpha'], df.loc[df['point_id'] == pos, 'curvature'], c='r')
+    ax[3].scatter(df.loc[df['point_id'] == neg, 'alpha'], df.loc[df['point_id'] == neg, 'curvature'], c='g')
+    plt.show()
 
 def delineate_valley(wbt, dem_raster, flowline, stream_raster, hillslope_raster, flow_dir_raster):
     flowline = flowline.simplify(10)
