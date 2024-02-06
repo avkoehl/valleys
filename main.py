ffd6193ee976801408b8d8f3ef19a03abd32d174
Arthur Koehl <avkoehl@ucdavis.edu>
2024-02-05 22:55:34 -0800

    iterate through

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 import geopandas as gpd
+import numpy as np
 import rioxarray
 
 from valleys.utils import setup_wbt
@@ -22,3 +23,8 @@ watershed = Watershed(dem, nhd_network, "./data/working/watershed/")
 watershed.process_watershed(wbt)
 
 # ------------ Valleys ------------
+s_ids = np.unique(watershed.dataset.subbasins.values)
+s_ids = s_ids[~np.isnan(s_ids)]
+for s_id in s_ids:
+    subbasin = watershed.clip_to_subbasin(s_id)
+
